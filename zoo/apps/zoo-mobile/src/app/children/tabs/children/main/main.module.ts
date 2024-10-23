76476a15b79afe03459706845410e11455aadc2d
vtyric <misha.torno@gmail.com>
2024-10-23 23:57:09 +0500

    fix

--- zoo/apps/zoo-mobile/src/app/children/tabs/children/main/main.module.ts
+++ zoo/apps/zoo-mobile/src/app/children/tabs/children/main/main.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-import { MainHeaderComponent } from './component/main-header/main-header.component';
+import { MainHeaderComponent } from './components/main-header/main-header.component';
 import { MainPage } from './pages/main/main-page.component';
 import { ExploreContainerComponentModule } from '../../../../components/explore-container/explore-container.module';
-import { MainCarouselComponent } from './component/carousel/main-carousel.component';
-import { NewsItemComponent } from './component/news-item/news-item.component';
-import { ContactsComponent } from './component/contacts/contacts.component';
+import { MainCarouselComponent } from './components/carousel/main-carousel.component';
+import { NewsItemComponent } from './components/news-item/news-item.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
 const routes: Routes = [
     {
