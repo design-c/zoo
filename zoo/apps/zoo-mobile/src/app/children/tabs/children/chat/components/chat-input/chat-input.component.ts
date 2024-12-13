5032b417434dfc7fa0516947a955e56c3d7a181d
ARTSOFTE\sdorokhov <sdorokhov@arsofte.ru>
2024-12-13 19:12:19 +0500

    add chat

--- zoo/apps/zoo-mobile/src/app/children/tabs/children/chat/components/chat-input/chat-input.component.ts
+++ zoo/apps/zoo-mobile/src/app/children/tabs/children/chat/components/chat-input/chat-input.component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Output, signal, ViewChild, WritableSignal } from '@angular/core';
+
+export interface IImage {
+    file: File;
+    preview: string;
+    loading: boolean
+}
 
 @Component({
     selector: 'app-chat-input',
@@ -7,9 +13,7 @@ import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, ViewChil
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChatInputComponent implements AfterViewInit {
-
-    protected questionText: string = '';
-    private readonly _minHeight = 24; // Минимальная высота
+    protected attachedImages: WritableSignal<IImage[]> = signal([]);
 
     @ViewChild('input')
     private readonly _textarea!: ElementRef;
@@ -20,24 +24,63 @@ export class ChatInputComponent implements AfterViewInit {
     @ViewChild('right')
     private readonly _rightIcon!: ElementRef;
 
+    @Output()
+    public readonly sent: EventEmitter<any> = new EventEmitter();
+
+    protected questionText = '';
+    private readonly _minHeight = 24;
+    private readonly _linesCount  = 5;
+
+
+
+
     public ngAfterViewInit(): void {
         this.adjustElementHeight();
     }
 
-    protected adjustElementHeight(): void {
-        const textarea = this._textarea.nativeElement as HTMLTextAreaElement;
-        const left = this._leftIcon.nativeElement;
-        const right = this._rightIcon.nativeElement;
-
-        textarea.style.height = 'auto'; // Сброс высоты перед пересчетом
-        const newHeight = Math.max(textarea.scrollHeight, this._minHeight);
-
-        this.updateHeight(textarea, newHeight);
-        this.updateHeight(left, newHeight);
-        this.updateHeight(right, newHeight);
+    protected removeImage(index: number): void {
+        const updatedImages = [...this.attachedImages()];
+        updatedImages.splice(index, 1);
+        this.attachedImages.set(updatedImages);
     }
 
-    private updateHeight(element: HTMLElement, height: number): void {
-        element.style.height = `${ height }px`;
+    protected attachImage(event: Event): void {
+        const input = event.target as HTMLInputElement;
+        if (input.files && input.files[0]) {
+            const file = input.files[0];
+            const reader = new FileReader();
+
+            reader.onload = () => {
+                const currentImages = this.attachedImages();
+                const newImage: IImage = {
+                    file,
+                    preview: reader.result as string,
+                    loading: true // Изначально показываем лоадер
+                };
+
+                this.attachedImages.set(currentImages.concat(newImage));
+
+                // Эмуляция загрузки (можно заменить на реальную логику)
+                setTimeout(() => {
+                    const updatedImages = this.attachedImages().map((image) =>
+                        image === newImage ? { ...image, loading: false } : image
+                    );
+                    this.attachedImages.set(updatedImages);
+                }, 2000); // Загрузка длится 2 секунды
+            };
+
+            reader.readAsDataURL(file);
+        }
+    }
+
+    protected send(): void {
+        this.sent.emit();
+    }
+
+    protected adjustElementHeight(): void {
+        const textarea = this._textarea.nativeElement as HTMLTextAreaElement;
+        textarea.style.height = 'auto'; // Сброс высоты перед пересчетом
+        const newHeight = Math.min(Math.max(textarea.scrollHeight, this._minHeight), this._minHeight * this._linesCount);
+        textarea.style.height = `${ newHeight }px`;
     }
 }
